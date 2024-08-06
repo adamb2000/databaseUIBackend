f6f1b8fe0adc9cecca350f838940b53cf5f57a6f
adamb2000 <adam-b130@hotmail.com>
2024-08-06 20:47:40 +0100

    error handling and logout

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #flask --app main run --debug
-from flask import Flask, request, abort
+from sqlalchemy import exc
+from flask import Flask, request, abort, jsonify
 from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+import json
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -30,31 +32,60 @@ class Users(UserMixin, db.Model):
 with app.app_context():
     db.create_all()
 
+data = open('./programText/ApiErrorMessages.json')
+errorResponses = json.load(data)
+
 @app.route("/register", methods=['POST'])
 @cross_origin(supports_credentials=True)
 def register():
-    username = request.json['username']
-    password = request.json['password']
-    user = Users(username=username,password=password)
-    db.session.add(user)
-    db.session.commit()
-    return  {
-        "username": username,
-        "token": "token123",
-        "authenticated": True
-    }
- 
+    try:
+        username = request.json['username']
+        password = request.json['password']
+        user = Users(username=username,password=password)
+        db.session.add(user)
+        db.session.commit()
+        return  {
+            "username": username,
+            "token": "token123",
+            "authenticated": True
+        }
+    except exc.IntegrityError:
+        return handleErrorResponse('duplicateAccount',403)
+    except:
+        return handleErrorResponse('unhandledError',500)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 @app.route("/login", methods=['POST'])
 @cross_origin(supports_credentials=True)
 def login():
-    username = request.json['username']
-    password = request.json['password']
-    user = Users.query.filter_by(username=username).first()
-    print(user)
-    if user.password == password:
-        login_user(user)
-        return {
-            "auth":True
-        }
-    else:
-        abort(403)
+    try:
+        username = request.json['username']
+        password = request.json['password']
+        user = Users.query.filter_by(username=username).first()
+        if user:
+            if user.password == password:
+                if login_user(user):
+                    response = jsonify({'id': user.id, 'username': username})
+                    response.status_code = 200
+                    return response
+                else:
+                    return handleErrorResponse('inactiveAccount',403)
+            else:
+                return handleErrorResponse('incorrectPassword',403)
+        else:
+            return handleErrorResponse('userNotFound',403)
+    except:
+        return handleErrorResponse('unhandledError',500)
+    
+@app.route('/logout',methods=['POST'])
+@cross_origin(supports_credentials=True)
+@login_required
+def logout():
+    logout_user()
+    response = jsonify({'auth':False})
+    response.status_code = 200
+    return response
+
+def handleErrorResponse(message, code):
+    response = jsonify({'message': errorResponses[message]})
+    response.status_code = code
+    return response
